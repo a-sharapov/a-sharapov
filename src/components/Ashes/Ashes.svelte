af127d32d086532d7898496ddadabf7f6d0e7a81
Alexander Sharapov <mail@a-sahrapov.com>
2023-09-18 21:52:05 +0300

    Update

--- src/components/Ashes/Ashes.svelte
+++ src/components/Ashes/Ashes.svelte
@@ -8,12 +8,56 @@
     $ashes.push(i);
     i++;
   }
-
-  import styles from "./Ashes.module.scss";
 </script>
 
-<div class={styles.ashes}>
+<div class="ashes">
   {#each $ashes as ash}
     <i data-item={ash} />
   {/each}
 </div>
+
+<style lang="scss">
+  .ashes {
+    position: fixed;
+    width: calc(100% + 2.5rem);
+    height: calc(100% + 2.5rem);
+    offset: -5rem;
+    overflow: visible;
+    transform: scale(1, -1);
+    pointer-events: none;
+    user-select: none;
+    transform: translateZ(500px);
+    z-index: 1000;
+
+    & > i {
+      position: absolute;
+      width: 1px;
+      height: 1px;
+      background: #999;
+      border-radius: 50%;
+      user-select: none;
+      will-change: auto;
+      box-shadow: 0 0 3px #ccc5, 0 0 5px #ccc1;
+    }
+  }
+
+  @keyframes ashFall {
+    0% {
+      transform: translate3d(var(--left-ini), 110vh, 0);
+    }
+    100% {
+      transform: translate3d(var(--left-end), 0, 0);
+    }
+  }
+
+  @for $i from 1 through 200 {
+    .ashes > i:nth-child(#{$i}) {
+      --size: #{random(5) * 0.2}vw;
+      --left-ini: #{random(20) - 10}vw;
+      --left-end: #{random(20) - 10}vw;
+      left: #{random(100)}vw;
+      animation: ashFall #{10 + random(10)}s linear infinite;
+      animation-delay: -#{random(10)}s;
+    }
+  }
+</style>
