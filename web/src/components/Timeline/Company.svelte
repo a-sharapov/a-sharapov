d86005341b75bc633734e6cc9c26d698d0ef9c02
Alexander Sharapov <a-sharapov@outlook.com>
2024-08-02 23:19:34 +0300

    Updates

--- web/src/components/Timeline/Company.svelte
+++ web/src/components/Timeline/Company.svelte
@@ -9,9 +9,10 @@
   export let timetable
   export let logoPath = '/images/companies/'
 
-  let tt_text_description = timetable
+  let ttTextDescription = timetable
     .split(',')
     .map((tt) => LOCALES[$store.locale].TIMETABLE[tt.trim()])
+    .filter(Boolean)
     .join(', ')
 </script>
 
@@ -20,7 +21,7 @@
     {start}&nbsp;&ndash;&nbsp;{end}
   </dis>
   <div class="roleDescription">
-    <span class="title">{role}</span><span class="timetable">,&nbsp;{tt_text_description} </span>
+    <span class="title">{role}</span><span class="timetable">,&nbsp;{ttTextDescription} </span>
     {#if companyName}
       {#await import(/* @vite-ignore */ `${logoPath}${companyName}.svg?raw`) then { default: logo }}
         <span class="at">&commat;</span>
